978ab700cdd3158843a853c06bfc4c7426df73e6
Matthias Fuchs <matz.fuchs@gmx.at>
2020-09-27 21:23:19 +0200

    Crawler clones repos

--- services/crawler/swagger_server/config.py
+++ services/crawler/swagger_server/config.py
@@ -4,7 +4,8 @@ import logging
 import os
 
 app = connexion.App(__name__, specification_dir='./swagger/')
-app.app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://root:%s@127.0.0.1/conan-ci' % os.environ['MYSQL_ROOT_PASSWORD']
+app.app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+mysqldb://root:%s@127.0.0.1/conan-ci'
+                                             % os.environ.get('MYSQL_ROOT_PASSWORD', ''))
 app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app.app)
 logging.basicConfig(level=logging.INFO)
--- services/crawler/swagger_server/crawler/process.py
+++ services/crawler/swagger_server/crawler/process.py
@@ -1,10 +1,12 @@
 from swagger_server.config import app
-from swagger_server import database 
+from swagger_server import database
 import git
 import os.path
+import re
 import shutil
 
 data_dir = os.environ.get("CRAWLER_REPO_DIR", "/data")
+logger = app.app.logger
 
 
 class RepoController(object):
@@ -29,25 +31,45 @@ class RepoController(object):
         shutil.rmtree(self.repo_dir, ignore_errors=True)
         repo = git.Repo.clone_from(url=url, to_path=self.repo_dir)
 
+    def fetch(self):
+        repo = git.Repo(self.repo_dir)
+        repo.git.fetch()
+
+    def get_remote_branches(self):
+        repo = git.Repo(self.repo_dir)
+        branches = [b.strip() for b in repo.git.branch('-r').split()]
+        pattern = 'origin/([/\-\\w]+)'
+        matches = [re.match(pattern, b) for b in branches]
+        return list(set(m.group(1) for m in matches if m and m.group(1) != 'HEAD'))
+
+    def checkout_and_update(self, branch):
+        repo = git.Repo(self.repo_dir)
+        repo.git.checkout(branch)
+        repo.git.pull()
 
 def trigger():
-    app.app.logger.info("start crawling")
+    logger.info("start crawling")
 
     if not os.path.exists(data_dir):
         os.makedirs(data_dir, exist_ok=True)
-        app.app.logger.info("created directory '%s'", data_dir)
+        logger.info("created directory '%s'", data_dir)
 
     repos = database.Repo.query.all()
+    channels = database.Channel.query.all()
     for repo in repos:
         repo_dir = os.path.join(data_dir, str(repo.id))
         controller = RepoController(repo_dir)
         if not controller.is_clone_of(repo.url):
             controller.create_new_clone(repo.url)
-            app.app.logger.info("cloned URL '%s' to '%s'", repo.url, repo_dir)
+            logger.info("cloned URL '%s' to '%s'", repo.url, repo_dir)
         else:
-            app.app.logger.debug("use existing repo '%s' for URL '%s'",
-                                 repo_dir, repo.url)
+            controller.fetch()
+            logger.info("fetch existing repo '%s' for URL '%s'",
+                         repo_dir, repo.url)
 
-
-
+        branches = controller.get_remote_branches()
+        for channel in channels:
+            if channel.branch in branches:
+                controller.checkout_and_update(channel.branch)
+                logger.info("checkout and update branch '%s'", channel.branch)
 
