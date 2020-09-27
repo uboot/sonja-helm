3a7a92cebb16ccccd4d7595bf1f86b512e77648e
Matthias Fuchs <matz.fuchs@gmx.at>
2020-09-27 21:23:20 +0200

    Add public service to kubernetes

--- services/crawler/swagger_server/__main__.py
+++ services/crawler/swagger_server/__main__.py
@@ -7,7 +7,7 @@ def main():
     app.add_api('swagger.yaml', arguments={'title': 'Conan CI Crawler'}, pythonic_params=True)
     app.app.json_encoder = encoder.JSONEncoder
     db.create_all()
-    app.run(port=8081)
+    app.run(port=8080)
 
 if __name__ == '__main__':
     main()
--- services/public/swagger_server/config.py
+++ services/public/swagger_server/config.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = connexion.App(__name__, specification_dir='./swagger/')
-app.app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+mysqldb://root:%s@127.0.0.1/conan-ci'
+app.app.config['SQLALCHEMY_DATABASE_URI'] = ('mysql+mysqldb://root:%s@mysql/conan-ci'
                                              % os.environ.get('MYSQL_ROOT_PASSWORD', ''))
 app.app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app.app)
