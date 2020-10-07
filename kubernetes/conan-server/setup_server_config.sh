a18fd37c1f527555af47707ef9ac14e859e65cdd
Matthias Fuchs <matz.fuchs@gmx.at>
2020-10-07 22:23:05 +0200

    Store conan server config in config map

--- kubernetes/conan-server/setup_server_config.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-wget "https://raw.githubusercontent.com/uboot/conan-ci/master/kubernetes/conan-server/server.conf.in" \
-&& sed -e "s/SECRET/$CONAN_USER_PASSWORD/g" /config/server.conf.in > /config/server.conf \
-&& rm /config/server.conf.in \
-&& rm /config/setup_server_config.sh
