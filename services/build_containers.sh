e7031317e674958794eb0b21fc5d1800c0427bcf
Matthias Fuchs <matz.fuchs@gmx.at>
2021-01-28 22:52:43 +0100

    Add frontend service to helm chart

--- services/build_containers.sh
+++ services/build_containers.sh
@@ -5,4 +5,5 @@ DIRECTORY=`dirname $0`
 docker build -t crawler:local -f $DIRECTORY/Dockerfile.crawler $DIRECTORY \
 && docker build -t agent:local -f $DIRECTORY/Dockerfile.agent $DIRECTORY/ \
 && docker build -t public:local -f $DIRECTORY/Dockerfile.public $DIRECTORY/ \
-&& docker build -t scheduler:local -f $DIRECTORY/Dockerfile.scheduler $DIRECTORY/
+&& docker build -t scheduler:local -f $DIRECTORY/Dockerfile.scheduler $DIRECTORY/ \
+&& docker build -t frontend:local -f $DIRECTORY/Dockerfile.frontend $DIRECTORY/
