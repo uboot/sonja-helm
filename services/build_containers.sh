1d195d01970d89cf6ec21de579cd661ce15188c6
Matthias Fuchs <matz.fuchs@gmx.at>
2021-02-07 22:37:04 +0100

    Use container images from docker hub

--- services/build_containers.sh
+++ services/build_containers.sh
@@ -2,8 +2,13 @@
 
 DIRECTORY=`dirname $0`
 
-docker build -t crawler:local -f $DIRECTORY/Dockerfile.crawler $DIRECTORY \
-&& docker build -t agent:local -f $DIRECTORY/Dockerfile.agent $DIRECTORY/ \
-&& docker build -t public:local -f $DIRECTORY/Dockerfile.public $DIRECTORY/ \
-&& docker build -t scheduler:local -f $DIRECTORY/Dockerfile.scheduler $DIRECTORY/ \
-&& docker build -t frontend:local -f $DIRECTORY/Dockerfile.frontend $DIRECTORY/
+   docker build -t uboot/crawler:latest -f $DIRECTORY/Dockerfile.crawler $DIRECTORY \
+&& docker build -t uboot/agent:latest -f $DIRECTORY/Dockerfile.agent $DIRECTORY \
+&& docker build -t uboot/public:latest -f $DIRECTORY/Dockerfile.public $DIRECTORY \
+&& docker build -t uboot/scheduler:latest -f $DIRECTORY/Dockerfile.scheduler $DIRECTORY \
+&& docker build -t uboot/frontend:latest -f $DIRECTORY/Dockerfile.frontend $DIRECTORY \
+&& docker push uboot/crawler:latest \
+&& docker push uboot/agent:latest \
+&& docker push uboot/public:latest \
+&& docker push uboot/scheduler:latest \
+&& docker push uboot/frontend:latest
