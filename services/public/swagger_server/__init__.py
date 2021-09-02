91e03f4af214dea7d56959d26422f58e6b23b10c
Matthias Fuchs <matz.fuchs@gmx.at>
2021-09-02 10:23:57 +0200

    Implement factory for public service

---
+++ services/public/swagger_server/__init__.py
@@ -0,0 +1,14 @@
+from conanci.config import app, connect_to_database, setup_logging
+from swagger_server import encoder, auth
+import os.path
+
+swagger_api = os.path.join(os.path.dirname(__file__), 'swagger', 'swagger.yaml')
+
+
+def create_app():
+    app.add_api(swagger_api, arguments={'title': 'Conan CI Linux Agent'}, pythonic_params=True)
+    app.app.json_encoder = encoder.JSONEncoder
+    setup_logging()
+    auth.setup_login(app.app)
+    connect_to_database()
+    return app
--- services/public/swagger_server/__main__.py
+++ services/public/swagger_server/__main__.py
@@ -1,18 +1,6 @@
 #!/usr/bin/env python3
 
-from conanci.config import app, connect_to_database, setup_logging
-from swagger_server import encoder, auth
-import os.path
-
-swagger_api = os.path.join(os.path.dirname(__file__), 'swagger', 'swagger.yaml')
-
-def main():
-    app.add_api(swagger_api, arguments={'title': 'Conan CI Linux Agent'}, pythonic_params=True)
-    app.app.json_encoder = encoder.JSONEncoder
-    setup_logging()
-    auth.setup_login(app.app)
-    connect_to_database()
-    app.run(port=8080)
+from swagger_server import app
 
 if __name__ == '__main__':
-    main()
+    app.run(port=8080)
