82a5cac9b61e5e1a0c9131d31a993e74243f4916
Matthias Fuchs <matz.fuchs@gmx.at>
2021-09-06 22:28:48 +0200

    Use gunicorn for public API

--- services/public/swagger_server/__init__.py
+++ services/public/swagger_server/__init__.py
@@ -6,7 +6,7 @@ swagger_api = os.path.join(os.path.dirname(__file__), 'swagger', 'swagger.yaml')
 
 
 def create_app():
-    app.add_api(swagger_api, arguments={'title': 'Conan CI Linux Agent'}, pythonic_params=True)
+    app.add_api(swagger_api, arguments={'title': 'Conan CI Public API'}, pythonic_params=True)
     app.app.json_encoder = encoder.JSONEncoder
     setup_logging()
     auth.setup_login(app.app)
--- services/public/swagger_server/__main__.py
+++ services/public/swagger_server/__main__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
-from swagger_server import app
+from swagger_server import create_app
+
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(port=8080)
