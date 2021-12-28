527c5b18e649da9c7404e4c6f7f395545b3bdf38
Matthias Fuchs <matz.fuchs@gmx.at>
2021-12-28 20:31:06 +0100

    Add docker build action

--- services/build_containers.sh
+++ services/build_containers.sh
@@ -6,9 +6,7 @@ DIRECTORY=`dirname $0`
 && docker build -t uboot/agent:latest -f $DIRECTORY/Dockerfile.agent $DIRECTORY \
 && docker build -t uboot/public:latest -f $DIRECTORY/Dockerfile.public $DIRECTORY \
 && docker build -t uboot/scheduler:latest -f $DIRECTORY/Dockerfile.scheduler $DIRECTORY \
-&& docker build -t uboot/frontend:latest -f $DIRECTORY/Dockerfile.frontend $DIRECTORY \
 && docker push uboot/crawler:latest \
 && docker push uboot/agent:latest \
 && docker push uboot/public:latest \
-&& docker push uboot/scheduler:latest \
-&& docker push uboot/frontend:latest
+&& docker push uboot/scheduler:latest
